604706069c538d1a122aeed33108b69af74aaf58
DIGITALMAHESH720 <124596441+DIGITALMAHESH720@users.noreply.github.com>
2024-04-22 10:52:26 +0530

    Update home.js

--- home.js
+++ home.js
@@ -1,1 +1,20 @@
-aefjhpondbigfv n
+<html >
+<head>
+ 
+  <title>MY HOME</title>
+</head>
+<body>
+  <div class="container">
+    <h1>Welcome to Our Shopping App</h1>
+
+    <div class="navigation">
+      <ul>
+        <li><a href="reg.html">Registration</a></li>
+        <li><a href="login.html">Login page</a></li>
+        <li><a href="catalog4.html">Catalog</a></li>
+        <li><a href="cart4.html">Cart</a></li>
+      </ul>
+    </div>
+  </div>
+</body>
+</html>
